d25a5be0485a5a55fa90feb3977f355d95985b0b
Andrew Hull <aghull@gmail.com>
2013-04-22 17:31:01 -0400

    libxml hack

--- dash/app/overrides/analytics_header.rb
+++ dash/app/overrides/analytics_header.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => Spree::Config[:layout],
-                     :name => "add_analytics_header",
-                     :insert_bottom => "head",
-                     :partial => "spree/analytics/header",
-                     :original => '6f23c8af6e863d0499835c00b3f2763cb98e1d75')
+# commenting out since this is screwing up deface in mac osx using libxml 2.9 - this should be restored when this is no longer an issue
+
+# Deface::Override.new(:virtual_path => Spree::Config[:layout],
+#                      :name => "add_analytics_header",
+#                      :insert_bottom => "head",
+#                      :partial => "spree/analytics/header",
+#                      :original => '6f23c8af6e863d0499835c00b3f2763cb98e1d75')
